5c7ed191123c09ceb3a60355105f2fcb6f5b897d
Kajus Kevevari <kajus.kevevari@wilibox.com>
2018-09-20 09:55:26 +0300

    done

--- Server.c
+++ Server.c
@@ -1,12 +1,12 @@
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
+//#include <sys/socket.h>
+//#include <netinet/in.h>
+//#include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+//#include <unistd.h>
 #include <errno.h>
 #include <string.h>
-#include <sys/types.h>
+//#include <sys/types.h>
   
 typedef struct Client* ClientPtr;
   
